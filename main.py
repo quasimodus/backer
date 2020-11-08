ca28944efb39c77366deaaa058cb975a18a8f799
quasimodus <diablero797@gmail.com>
2020-11-08 23:34:49 +0300

    add zip to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
+import time
+import zipfile
+
 from ui_backer import Ui_Dialog
 from to_arhiv import *
 
@@ -15,16 +17,38 @@ class CurrencyConv(QtWidgets.QDialog):
 
     def init_UI(self):
         self.setWindowTitle("backer_tool")
-        self.ui.toolButton_1.clicked.connect(self.get_puth)
+        self.ui.toolButton_1.clicked.connect(self.get_source)
 
-    def get_puth(self):
+    def get_source(self):
         # input_currency = self.ui.lineEdit.text()
         # # output_currency = self.ui.output_currency.text()
         # p = check_ping(input_currency)
 
-        wb_patch = QtWidgets.QFileDialog.getExistingDirectory()
-        self.ui.lineEdit_1.setText(wb_patch)
+        source = QtWidgets.QFileDialog.getExistingDirectory()
+        self.ui.lineEdit_1.setText(source)
         # print("Путь к файлу : " + wb_patch)
+        # source = "C:\\TEST_1C\\1C\\"
+
+        target_dir = 'D:\\__BACKUP__'
+
+        if not os.path.exists(target_dir):
+            os.mkdir(target_dir)  # создание каталога
+            print('Каталог успешно создан', target_dir)
+
+        today = target_dir + os.sep + time.strftime('%Y%m%d')
+        now = time.strftime('%H%M%S')
+        target = today + os.sep + now + '.zip'
+
+        if not os.path.exists(today):
+            os.mkdir(today)  # создание каталога
+            print('Каталог успешно создан', today)
+        mode = zipfile.ZIP_DEFLATED
+        zipp = zipfile.ZipFile(target, 'w', mode)  # создание архива
+        for root, dirs, files in os.walk(source):  # получаем адрес каталога и имена подкатологов и файлов
+            for file in files:
+                zipp.write(os.path.join(root, file))  # пишем файлы в архив
+
+        zipp.close()
 
 
 app = QtWidgets.QApplication([])
