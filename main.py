4070fc22a84ee22df433275447284ab0f2444cf5
quasimodus <diablero797@gmail.com>
2020-11-09 12:01:04 +0300

    09 morning

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
+import time
+import zipfile
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ui_backer import Ui_Dialog
+
+
 # from to_arhiv import *
-from to_arhiv import arhivator
+# from to_arhiv import arhivator
 
 
 class CurrencyConv(QtWidgets.QDialog):
@@ -18,6 +24,7 @@ class CurrencyConv(QtWidgets.QDialog):
         self.setWindowTitle("backer_tool")
         self.ui.toolButton_1.clicked.connect(self.get_source)
         self.ui.toolButton_2.clicked.connect(self.get_target)
+        self.ui.pushButton_1.clicked.connect(self.arhivator)
 
     def get_source(self):
         # input_currency = self.ui.lineEdit.text()
@@ -28,8 +35,9 @@ class CurrencyConv(QtWidgets.QDialog):
         self.ui.lineEdit_1.setText(source)
         # print("Путь к файлу : " + wb_patch)
 
-        # target_dir = QtWidgets.QFileDialog.getExistingDirectory()
-        target_dir = "D:\\_____BACKUP"
+        # source = QtWidgets.QFileDialog.getExistingDirectory()
+        # target_dir = "D:\\_____BACKUP"
+        print('source run')
         return source
 
     def get_target(self):
@@ -38,14 +46,41 @@ class CurrencyConv(QtWidgets.QDialog):
         # p = check_ping(input_currency)
 
         target_dir = QtWidgets.QFileDialog.getExistingDirectory()
-        self.ui.lineEdit_1.setText(target_dir)
+        self.ui.lineEdit_2.setText(target_dir)
         # print("Путь к файлу : " + wb_patch)
 
         # target_dir = QtWidgets.QFileDialog.getExistingDirectory()
         # target_dir = "D:\\_____BACKUP"
+
+        print('target run')
         return target_dir
 
-    arhivator(get_source, get_target) # не работает тут
+    # source = "D:\\test"
+    # target_dir = "D:\\_____BACKUP"
+
+    def arhivator(self):
+
+        if not os.path.exists(self.get_target()):
+            os.mkdir(self.get_target())  # создание каталога
+            print('Каталог успешно создан', self.get_target())
+
+        today = self.get_target() + os.sep + time.strftime('%Y%m%d')
+
+        now = time.strftime('%H%M%S')
+
+        target = today + os.sep + now + '.zip'
+
+        if not os.path.exists(today):
+            os.mkdir(today)  # создание каталога
+            print('Каталог успешно создан', today)
+        mode = zipfile.ZIP_DEFLATED
+        zipp = zipfile.ZipFile(target, 'w', mode)  # создание архива
+        for root, dirs, files in os.walk(self.get_source()):  # получаем адрес каталога и имена подкатологов и файлов
+            for file in files:
+                zipp.write(os.path.join(root, file))  # пишем файлы в архив
+
+        zipp.close()
+        print(self.get_target() + '\n' + self.get_source())
 
 
 app = QtWidgets.QApplication([])
--- to_arhiv.py
+++ to_arhiv.py
@@ -8,28 +8,28 @@ import zipfile
 import zlib
 
 
-def arhivator(source, target_dir):
-    # source = "C:\\TEST_1C\\1C\\"
-    #
-    # target_dir = 'E:\\BACKUP'  # Подставьте тот путь, который вы будете использовать.
-
-    if not os.path.exists(target_dir):
-        os.mkdir(target_dir)  # создание каталога
-        print('Каталог успешно создан', target_dir)
-
-    today = target_dir + os.sep + time.strftime('%Y%m%d')
-
-    now = time.strftime('%H%M%S')
-
-    target = today + os.sep + now + '.zip'
-
-    if not os.path.exists(today):
-        os.mkdir(today)  # создание каталога
-        print('Каталог успешно создан', today)
-    mode = zipfile.ZIP_DEFLATED
-    zipp = zipfile.ZipFile(target, 'w', mode)  # создание архива
-    for root, dirs, files in os.walk(source):  # получаем адрес каталога и имена подкатологов и файлов
-        for file in files:
-            zipp.write(os.path.join(root, file))  # пишем файлы в архив
-
-    zipp.close()
+# def arhivator(source, target_dir):
+#     # source = "C:\\TEST_1C\\1C\\"
+#     #
+#     # target_dir = 'E:\\BACKUP'  # Подставьте тот путь, который вы будете использовать.
+#
+#     if not os.path.exists(target_dir):
+#         os.mkdir(target_dir)  # создание каталога
+#         print('Каталог успешно создан', target_dir)
+#
+#     today = target_dir + os.sep + time.strftime('%Y%m%d')
+#
+#     now = time.strftime('%H%M%S')
+#
+#     target = today + os.sep + now + '.zip'
+#
+#     if not os.path.exists(today):
+#         os.mkdir(today)  # создание каталога
+#         print('Каталог успешно создан', today)
+#     mode = zipfile.ZIP_DEFLATED
+#     zipp = zipfile.ZipFile(target, 'w', mode)  # создание архива
+#     for root, dirs, files in os.walk(source):  # получаем адрес каталога и имена подкатологов и файлов
+#         for file in files:
+#             zipp.write(os.path.join(root, file))  # пишем файлы в архив
+#
+#     zipp.close()
